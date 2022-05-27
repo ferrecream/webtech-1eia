c46872a2ab4138e22fb10e8fb344695b799f9979
ferrecream <ferrecreemers@gmail.com>
2022-05-27 17:17:02 +0200

    update

--- mysql.php
+++ mysql.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "joske";
 $password = "appeltaart";
-$dbname = "persons";
+$dbname = "joske";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,14 +11,27 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
+$sql = "INSERT INTO persons (voornaam, achternaam, stad)
+VALUES ('Ferre', 'Creemers', 'Bree')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  echo "New record created successfully\n";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+$sql = "SELECT voornaam, achternaam, stad FROM persons";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+      echo " - voornaam: " . $row["voornaam"]. " - achternaam: " . $row["achternaam"]. " - stad" . $row["stad"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
 
 $conn->close();
 ?>
